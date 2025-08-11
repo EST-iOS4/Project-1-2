a4a6da06fdbaba4f7461c69dec59f4f384715219
JaeLee <jyl0422@gmail.com>
2025-08-11 13:27:27 +0900

    프로젝트 파일 업로드

---
+++ Remembrance/Remembrance/RemembranceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemembranceApp.swift
+//  Remembrance
+//
+//  Created by JaeyoungLee on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct RemembranceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
