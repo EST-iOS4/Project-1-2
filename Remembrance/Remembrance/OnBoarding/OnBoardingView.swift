dbe4b6ce982fb241fc6f054aa8d48dc4371953ef
JaeLee <jyl0422@gmail.com>
2025-08-11 14:47:35 +0900

    온보딩 뷰 파일 추가

---
+++ Remembrance/Remembrance/OnBoarding/OnBoardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnBoardingView.swift
+//  Remembrance
+//
+//  Created by JaeyoungLee on 8/11/25.
+//
+
+import SwiftUI
+
+struct OnBoardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnBoardingView()
+}
