339cf60388599da149ef83fb4be82a3260826470
JaeLee <jyl0422@gmail.com>
2025-08-11 16:12:35 +0900

    회고 디테일 뷰 파일 추가

---
+++ Remembrance/Remembrance/ContentList/ListDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ListDetailView.swift
+//  Remembrance
+//
+//  Created by JaeyoungLee on 8/11/25.
+//
+
+import SwiftUI
+
+struct ListDetailView: View {
+    var body: some View {
+        Text("ListDetailView")
+    }
+}
+
+#Preview {
+    ListDetailView()
+}
